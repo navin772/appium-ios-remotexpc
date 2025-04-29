9f95c61e7d724ff1dd6357a3644c2537dab398f4
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2025-04-29 18:31:24 +0530

    Rename Services/IOS to services/ios for case consistency

--- src/Services/index.ts
+++
@@ -1,5 +0,0 @@
-import * as diagnostics from './ios/diagnostic-service/index.js';
-import * as syslog from './ios/syslog-service/index.js';
-import * as tunnel from './ios/tunnel-service/index.js';
-
-export { diagnostics, syslog, tunnel };
